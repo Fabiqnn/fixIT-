2656326f2d16a01289109d59720ced2d9ddbbe80
Fabiqnn <arema20056@gmail.com>
2025-05-20 17:36:52 +0700

    landingPage FInale

--- resources/views/landingPage.blade.php
+++ resources/views/landingPage.blade.php
@@ -18,11 +18,11 @@
     </div>
 
     <div name="card2" class="p-20 w-full max-w-[1400px] mx-auto">
-        <div class="text-center">
+        <div class="text-center autoShow">
             <h1 class="font-bold text-4xl text-D_grey">Fitur Utama Pada Sistem fixIT!</h1>
             <h4>Yuk kenalan dengan fitur-fitur utama fixIT!</h4>
         </div>
-        <div class="flex text-center space-x-10 mt-20">
+        <div class="flex text-center space-x-10 mt-20 autoShow2">
             <div class="w-1/3 shadow-sm rounded space-y-5 px-3 py-5 ">
                 <i class="fa-solid fa-clipboard fa-2xl mt-5"></i>
                 <h1 class="text-D_grey font-bold text-2xl mt-5">Pelaporan <br> Kerusakan Fasilitas</h1>
@@ -42,7 +42,7 @@
         </div>
     </div>
 
-    <div name="card3" class="p-20 flex items-center gap-5 w-full max-w-[1400px] mx-auto">
+    <div name="card3" class="p-20 flex items-center gap-5 w-full max-w-[1400px] mx-auto autoShow">
         <div>
             <img src="{{ asset('assets/landingpage2.png') }}" alt="">
         </div>
@@ -56,7 +56,7 @@
     </div>
 
     <div class="bg-silver">
-        <div class="flex p-20 justify-between w-full max-w-[1400px] mx-auto">
+        <div class="flex p-20 justify-between w-full max-w-[1400px] mx-auto autoShow">
             <div>
                 <h1 class="text-4xl font-bold text-D_grey">Bersama, Rawat Kampus</h1>
                 <h1 class="text-4xl font-bold text-greenPrimary">Menjadi Lebih Baik</h1>
@@ -84,7 +84,7 @@
     </div>
 
     <div class="bg-silver">
-        <div class="p-20 space-y-3 w-full max-w-[1400px] mx-auto">
+        <div class="p-20 space-y-3 w-full max-w-[1400px] mx-auto autoShow2">
             <h1 class="text-4xl font-bold text-D_grey">Kampus lebih baik dimulai dari langkah kecil.</h1>
             <h4 class="text-xl">Yuk, mulai dari laporanmu hari ini!.</h4>
             <button class="bg-greenPrimary text-white px-[16px] py-[8px] rounded hover:bg-greenshade1 transition items-center mt-5">Masuk</button>
